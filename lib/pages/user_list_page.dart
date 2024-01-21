7ea08625e1297b460aa916906c486d91bc780d6c
umar200000 <ruzieumar19@gmail.com>
2024-01-21 17:01:49 +0500

    new app

---
+++ lib/pages/user_list_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UserListPage extends StatefulWidget {
+  const UserListPage({super.key});
+
+  @override
+  State<UserListPage> createState() => _UserListPageState();
+}
+
+class _UserListPageState extends State<UserListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(),
+    );
+  }
+}
